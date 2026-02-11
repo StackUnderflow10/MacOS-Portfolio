6147274b37229b90483c13c078a3bdd8ef1d49a1
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2026-02-11 21:31:10 +0530

    fixed return state runtime potential error

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -16,7 +16,7 @@ const renderText = (text, className, baseWeight = 400) => {
 }
 
 const setupTextHover = (container,type) => {
-    if(!container) return;
+    if(!container) return() => {};
 
     const letters = container.querySelectorAll('span');
     const { min, max, default: base } = FONT_WEIGHTS[type];
@@ -71,7 +71,7 @@ const Welcome = () => {
         <h1 ref={titleRef} className='mt-7'>{renderText("portfolio",'text-9xl italic font-georama')}</h1>
 
         <div className='small-screen'>
-            <p>This Portfolio is designed for desktop/tabled screens only</p>
+            <p>This Portfolio is designed for desktop/tablet screens only</p>
         </div>
     </section>
   )
